5f823230bf00093f92adb6ac4798026dcd14379c
tobiasnf <tobiasnf@gmail.com>
2020-06-22 12:54:59 +0200

    done

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
+let winner = document.querySelector("#winner")
+let theTurn = document.querySelector('#theTurn')
+
 let nextTurn = 'X';
 let board = [
     ['', '', ''],
@@ -6,54 +9,34 @@ let board = [
     ['', '', ''],
 ]
 
-function clearEverything() {
-    document.getElementById("winner").innerText = " ";
-    for (let x = 0; x < 3; x++) {
-        for (let y = 0; y < 3; y++) {
-            board[x][y] = " ";
-
-        }
-    }
-    displayBoard();
-}
-
 function myClick(x, y) {
-    console.log("test" + x + " " + y)
-    board[x][y] = nextTurn;
-    if (nextTurn == 'X') {
-        nextTurn = 'O';
+    if (board[x][y] == "") {
+        document.getElementById(x + '-' + y).classList.remove('tableSquare')
+        board[x][y] = nextTurn;
+        if (nextTurn == 'X') {
+            nextTurn = 'O';
+        } else {
+            nextTurn = 'X';
+        }
+        theTurn.innerText = 'Next turn:' + nextTurn;
+        displayBoard()
+        checkWinner()
     }
-    else {
-        nextTurn = 'X';
-    }
-    document.getElementById('theTurn').innerText = nextTurn;
-
-    console.log(board)
-    displayBoard()
-    checkWinner()
 }
-
 function displayBoard() {
     for (let x = 0; x < 3; x++) {
         for (let y = 0; y < 3; y++) {
-
             let id = x + "-" + y;
             if (board[x][y] == "O") {
-                document.getElementById(id).innerHTML = "<img src = 'Cirkel.png'></img>";
-            }
-            else if (board[x][y] == "X") {
-                document.getElementById(id).innerHTML = "<img src = 'Kryds.png'></img>";
-            }
-            else {
-                document.getElementById(id).innerHTML = "";
+                document.getElementById(id).innerHTML = "<img src='Cirkel.png'></img>";
+            } else if (board[x][y] == "X") {
+                document.getElementById(id).innerHTML = "<img src='Kryds.png'></img>";
             }
         }
     }
 }
-
 function checkWinner() {
-    if (
-        board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X" ||
+    if (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X" ||
         board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X" ||
         board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X" ||
         board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X" ||
@@ -62,10 +45,9 @@ function checkWinner() {
         board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X" ||
         board[2][0] == "X" && board[1][1] == "X" && board[0][2] == "X"
     ) {
-        document.getElementById("winner").innerText = "X wins!"
+        winner.innerText = "X wins!"
     }
-    if (
-        board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O" ||
+    if (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O" ||
         board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O" ||
         board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O" ||
         board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O" ||
@@ -74,6 +56,23 @@ function checkWinner() {
         board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O" ||
         board[2][0] == "O" && board[1][1] == "O" && board[0][2] == "O"
     ) {
-        document.getElementById("winner").innerText = "O wins!"
+        winner.innerText = "O wins!"
     }
+}
+function clearEverything() {
+    winner.innerText = "";
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+            board[x][y] = "";
+            let id = x + "-" + y;
+            let block = document.getElementById(id)
+            if (block.children.length > 0) {
+                block.removeChild(block.childNodes[0]);
+                block.style.border = "3px solid black"
+                block.style.borderRadius = "3%"
+            }
+            console.log(block.children)
+        }
+    }
+    displayBoard();
 }
